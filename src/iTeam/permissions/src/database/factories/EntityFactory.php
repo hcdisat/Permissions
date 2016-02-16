5fec08ae65fbd831646747f05abbcf82072f3ac2
vagrant <vagrant@homestead>
2016-02-16 15:54:38 +0000

    Added permmissions repo

---
+++ src/iTeam/permissions/src/database/factories/EntityFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use ITeam\Permissions\Models\Permission;
+use ITeam\Permissions\Models\Role;
+
+$factory->define(Role::class, function(\Faker\Generator $faker) : array {
+    return [
+        'name' => $word = $faker->word() . str_random(3),
+        'slug' => strtolower(snake_case($word, '')),
+        'description' => $faker->text()
+    ];
+});
+
+
+$factory->define(Permission::class, function(\Faker\Generator $faker) : array {
+    return [
+        'name' => $word = $faker->word() . str_random(3),
+        'slug' => strtolower(snake_case($word, '')),
+        'description' => $faker->text()
+    ];
+});
